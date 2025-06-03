d29f4fb9e528168c3f2cb02d2d4b9873bbe4aa97
Mateusz Baciak <m.baciak@muchmore.pl>
2025-06-03 17:01:40 +0200

    set wrong gtm id

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="GTM-TB77XH9H" />
+      {/* <GoogleTagManager gtmId="GTM-TB77XH9H" /> */}
+      <GoogleTagManager gtmId="GTM-TB77XH9A" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
